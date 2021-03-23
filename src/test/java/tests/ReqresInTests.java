7190bdbe63391c96377c770c9be749403214ad64
Valkyrja <kilyanet@gmail.com>
2021-03-23 23:55:37 +0300

    Simple reqres.io tests (homework)

--- src/test/java/tests/ReqresInTests.java
+++ src/test/java/tests/ReqresInTests.java
@@ -6,9 +6,10 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static io.restassured.RestAssured.*;
+import static io.restassured.path.json.JsonPath.from;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static utils.FileUtils.readStringFromFile;
 
 public class ReqresInTests {
@@ -71,4 +72,55 @@ public class ReqresInTests {
                 .log().body()
                 .body("error", is("Missing password"));
     }
+
+    //Homework tests
+
+    @Test
+    void checkUserHasAnEmailTest(){
+        given()
+            .when()
+                .get("/api/users/2")
+            .then()
+                .statusCode(200)
+                .log().body()
+                .body("data.email", is(notNullValue()));
+    }
+
+    @Test
+    void nonexistingUserNotFoundTest(){
+        get("/api/users/23")
+            .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void resourcesInListTest(){
+       get("/api/unknown").
+               then().statusCode(200)
+               .log().body()
+               .body("data", is(notNullValue()));
+    }
+
+    @Test // this test doesn't work as intended because the server returns the error answer. Maybe some temporary issue
+    void successfulLoginTest(){
+        given()
+                .body("{ \"email\": \"eve.holt@reqres.in\",\n \"password\": \"pistol\"\n }")
+                .when().post("/api/register")
+                .then().statusCode(400)
+                        .log().body()
+                        .body("id", is(not(0))).body("token", is(notNullValue()));
+    }
+
+    @Test
+    void unSuccessfulLoginTest(){
+        given()
+                .body("{\n" +
+                        "    \"email\": \"sydney@fife\"\n" +
+                        "}")
+                .when().post("/api/register")
+                .then().statusCode(400)
+                .log().body()
+                .body("error", is("Missing email or username"));
+    }
+
 }
